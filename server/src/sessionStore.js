fbe35ebdf93b1c852622e0f0f8474c795b3ca7a4
Elias Schuhmacher <eliaes@kth.se>
2024-10-03 11:50:04 +0200

    Switched to persistent session storage instead of MemoryStore

--- server/src/sessionStore.js
+++ server/src/sessionStore.js
@@ -1,5 +1,11 @@
 import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
 
-const sessionStore = new session.MemoryStore();
+const SQLiteStore = connectSqlite3(session);
+
+const sessionStore = new SQLiteStore({
+    db: 'sessions.sqlite', // Name of the SQLite database file
+    table: 'sessions', // Name of the table to store sessions
+  });
 
 export default sessionStore;
